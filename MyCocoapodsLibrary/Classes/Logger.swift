baed41c6114afbc2a062bb5f4d7b5fcc702bf97c
Rob Zmudzinski <rob@rob.com>
2022-07-29 09:12:47 -0700

    Initial Commit

---
+++ MyCocoapodsLibrary/Classes/Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  MyCocoapodsLibrary
+//
+//  Created by Rob Zmudzinski on 7/28/22.
+//
+
+import Foundation
+
+public class Logger {
+	public init() {}
+	public func printLog() {
+		print("Hello world")
+	}
+	func privateMethod() {
+		print("private")
+	}
+}
