204c46852cbf1714a78dd6f8c8c0c6a7a03bb59d
Rob Zmudzinski <rob@rob.com>
2022-07-30 10:33:09 -0700

    Adding class TestThree.swift

---
+++ MyCocoapodsLibrary/Classes/TestThree.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  MyCocoapodsLibrary
+//
+//  Created by Rob Zmudzinski on 7/28/22.
+//
+
+import Foundation
+
+public class TestThree {
+	public init() {}
+	public func doSomething() {
+		print("Doing Something")
+	}
+}
